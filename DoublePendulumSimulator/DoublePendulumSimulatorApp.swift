add125466b5c3f9d8ea4355a9aced547bdc44d33
Jayachandran Tanish <tanish.jaya@gmail.com>
2024-10-14 00:40:24 +0800

    Initial Commit

---
+++ DoublePendulumSimulator/DoublePendulumSimulatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoublePendulumSimulatorApp.swift
+//  DoublePendulumSimulator
+//
+//  Created by Tanish Jayachandran on 14/10/24.
+//
+
+import SwiftUI
+
+@main
+struct DoublePendulumSimulatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
